b340909d01a0c9332fe651b55d5208081c9c16af
Rok Kuzner <rok.ankaran@gmail.com>
2025-03-09 18:17:08 +0100

    Add selector for secondary content container in initialization

--- script.js
+++ script.js
@@ -79,6 +79,7 @@ async function initExtension() {
     let container_element = document.querySelector("#movie_player")
     let bottom_bar = document.querySelector("#movie_player > div.ytp-chrome-bottom")
     let related_videos_container = document.querySelector("#related")
+    let secondary_contnet_container = document.querySelector("#secondary")
     let bottom_content = document.querySelector("#below")
     let player_element = document.querySelector("#player")
 
