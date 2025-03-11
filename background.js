7ec9f283e1fa1bc79b3bfe0f652556bf9dd37482
Rok Kuzner <rok.ankaran@gmail.com>
2025-03-11 14:42:43 +0100

    Inject styles.css along with script.js on YouTube video page navigation

--- background.js
+++ background.js
@@ -1,14 +1,18 @@
 // background.js
 
-chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
     // Check if the URL is a YouTube video page
     if (details.url.includes("youtube.com/watch")) {
-      chrome.scripting.executeScript({
-        target: {tabId: details.tabId},
-        files: ['script.js'] // Path to your content script file
-      });
-      console.log("Content script injected into:", details.url);
+        chrome.scripting.executeScript({
+            target: { tabId: details.tabId },
+            files: ['script.js']
+        });
+        chrome.scripting.insertCSS({ // For injecting styles.css
+            target: { tabId: details.tabId },
+            files: ['styles.css']
+        });
+        console.log("Content script injected into:", details.url);
     }
-  });
-  
-  console.log("Background script running");
+});
+
+console.log("Background script running");
